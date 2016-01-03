c02e2f53b86ecb42ac286618ac9de5ff22806784
jxt1234 <jxt1234@zju.edu.cn>
2016-01-03 20:29:34 +0800

    Add Setup.py for python-renascence

---
+++ python-renascence/module/__init__.py
@@ -0,0 +1,2 @@
+import RenascenceBasic
+import Renascence
---
+++ python-renascence/module/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+from distutils.extension import Extension
+setup(
+        name = 'Renascence',
+        version = '0.1',
+        packages = find_packages(),
+        py_modules =['Renascence', 'RenascenceBasic'],
+        ext_modules=[Extension("_RenascenceBasic",
+            ["renascence_basic_wrap.c"], include_dirs=['../../include/'],
+            extra_link_args=["../../libGP.a"],
+            ),
+            ],
+        )
